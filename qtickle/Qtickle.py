562ff782dd50ef24ba4422608fa916272d1af60d
Nicholas Finch <ngf4@nau.edu>
2017-10-20 20:56:52 -0700

    Documentation update

--- qtickle/Qtickle.py
+++ qtickle/Qtickle.py
@@ -10,7 +10,13 @@ class Qtickle(object):
     A script designed to aid in saving values inside Qt objects.
     It works by inspecting for objectnames and then collecting their
     values.
+
+    The name is a portmanteau of Qt and pickle. Originally the script
+    utilized pickle to save data. Now data is returned in the form of
+    a dictionary. This way the user gets to choose what happens to the
+    data
     """
+
     def __init__(self, ui):
         self.ui = ui
 
